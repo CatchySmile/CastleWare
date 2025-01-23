625baf74ec5bd0be0c4402b9308a41f131b5c9d5
Sammy Folkhome <71086742+CatchySmile@users.noreply.github.com>
2025-01-23 01:35:37 +0000

    Create offsets.h

---
+++ offsets.h
@@ -0,0 +1,19 @@
+#include <vector>
+
+#pragma once
+
+// Offsets for the "Hide Name" feature
+const DWORD hideNameOffset = 0x1C0044;
+const BYTE originalHideNameBytes[] = { 0xC6, 0x86, 0x24, 0x12, 0x00, 0x00, 0x00 };
+const BYTE injectedHideNameBytes[] = { 0xC6, 0x86, 0x24, 0x12, 0x00, 0x00, 0x01 };
+
+// FOV offsets
+const DWORD offsetGameToBaseAddressFOV = 0x002F7A30;
+const std::vector<DWORD> pointsOffsets{ 0x30, 0x38, 0x298, 0x264, 0x10C, 0x3C, 0x4F4 };
+
+// Jump offsets
+const DWORD offsetGameToBaseAddressJump = 0x002FFE34;
+const std::vector<DWORD> customOffsets{ 0x19C, 0x1D8, 0x1AC, 0x1A4, 0x198, 0x34C, 0x478 };
+
+// Fast Menu offset
+const DWORD fastMenuOffset = 0x00C7EF88;
